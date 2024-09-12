3eca4f550c50509c1348ce847c3729ebdc077c1e
MohammedTarekT1M <95483676+MohammedTarekT1M@users.noreply.github.com>
2024-09-12 09:22:07 +0200

    js variabelen toevoegen

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let startPunten=('5');
+let startPoint=('5');
 // je krijgt 1 punt per keer dat je goed raad
 let puntenGain=('+1');
 const maxPunten = ('15');
@@ -6,12 +6,19 @@ const maxPunten = ('15');
 let puntenVerlies=('-1')
 // elke keer dat je de button pressed gaat die rollen
 let buttonPress=('roll')
+// druk op start button om de spel te beginnen 
 let startButtob=('start')
 // je hebt een sound button die aan en uit kan
 let volumeButton=('on/off')
-let hoger = ('hoger')
-let lager = ('lager')
-let dobbelSteen = ('dobbelSteen')
+// als je op hoger button drukt
+let heigherButton = ('hoger')
+// als je op lager button drukt
+let lowerButton = ('lager')
+// als je op dobbelsteen drukt
+let diceOne = ('dobbelSteen')
+// als je op restart button drukt
 const restartButton = ('restart')
+// als je op home button drukt
 const homeButton = ('homeButton')
-let score=('score')
+// hier zie je je best score
+let score=('score') 
