59c20c4d733041bd4b989fe05b33683b3d7e97da
MohammedTarekT1M <95483676+MohammedTarekT1M@users.noreply.github.com>
2024-09-12 09:10:21 +0200

    js variabelen

---
+++ main.js
@@ -0,0 +1,17 @@
+let startPunten=('5');
+// je krijgt 1 punt per keer dat je goed raad
+let puntenGain=('+1');
+const maxPunten = ('15');
+// je verliest 1 punt per keer dat je slecht raad
+let puntenVerlies=('-1')
+// elke keer dat je de button pressed gaat die rollen
+let buttonPress=('roll')
+let startButtob=('start')
+// je hebt een sound button die aan en uit kan
+let volumeButton=('on/off')
+let hoger = ('hoger')
+let lager = ('lager')
+let dobbelSteen = ('dobbelSteen')
+const restartButton = ('restart')
+const homeButton = ('homeButton')
+let score=('score')
