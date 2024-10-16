00523f514534dad877ec0e604374ef9de03cb8ea
MohammedTarekT1M <95483676+MohammedTarekT1M@users.noreply.github.com>
2024-10-16 12:56:39 +0200

    werken met gebruiker roll

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-// let startPoint=('5');
-// // je krijgt 1 punt per keer dat je goed raad
-// let puntenGain=('+1');
-// const maxPunten = ('10');
-// // je verliest 1 punt per keer dat je slecht raad
-// let puntenVerlies=('-1')
-// // elke keer dat je de button pressed gaat die rollen
-// let buttonPress=('roll')
-// // druk op start button om de spel te beginnen 
-// let startButtob=('start')
-// // je hebt een sound button die aan en uit kan
-// let volumeButton=('on/off')
-// // als je op hoger button drukt
-// let heigherButton = ('hoger')
-// // als je op lager button drukt
-// let lowerButton = ('lager')
-// // als je op dobbelsteen drukt
-// let diceOne = ('dobbelSteen')
-// // als je op restart button drukt
-// const restartButton = ('restart')
-// // als je op home button drukt
-// const homeButton = ('homeButton')
-// // hier zie je je best score
-// let score=('score') 
-// Math.random()
-
 // Selecteer elementen 
 const nameDisplay = document.querySelector('.name')
 const currentRollDisplay = document.querySelector('.current-roll')
@@ -31,6 +5,7 @@ const scoreDisplay = document.querySelector('.score')
 const livesDisplay = document.querySelector('.lives')
 const hogerButton = document.querySelector('.hoger')
 const lagerButton = document.querySelector('.lager')
+const myRoll = document.querySelector('.your-roll');
 
 // Variabelen initialiseren
 let currentRoll = diceRoll()
@@ -38,7 +13,7 @@ let score = 0;
 let lives = 5;
 let currentIndex = Math.floor(Math.random() * 6) + 1;
 
-//????
+// Bewaart de data van bestscore
 let bestScore = localStorage.getItem('bestScore') || 0;
 
 // Toon de beginrol en levens
@@ -57,7 +32,6 @@ function diceRoll(){
     return Math.floor(Math.random() * 6) + 1;
 }
 
-//console.log(Math.floor(Math.random() *6) + 1);
 
 // Controleer de gok van de speler
 function checkGuess(guess) {
@@ -78,7 +52,6 @@ function checkGuess(guess) {
 
     // Update de huidige rol, score en levens 
     currentRoll = newRoll;
-    
     scoreDisplay.textContent = score;
     livesDisplay.textContent = lives;
 
@@ -119,6 +92,7 @@ lagerButton.addEventListener('click', function(){
     checkGuess("lager");
 });
 
+// Array voor img
 const images = [
     {
         diceValue: 1,
@@ -146,13 +120,8 @@ const images = [
     }
 ];
 
-// let currentIndex = Math.floor(Math.random() * images.length) + 1;
-
 function changeImage() {
     const imageClass = document.querySelector('.dice1');
-
-    
-
     currentIndex = Math.floor(Math.random() * 6) + 1;
 
     for(let i = 0; i < images.length; i++){
